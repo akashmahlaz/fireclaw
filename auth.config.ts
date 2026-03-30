53046a8be6de6bb2b7f157c2f042febe9eb85506
akashmahlaz <akashdalla408@gmail.com>
2026-03-30 08:05:57 +0530

    revomed bad code

--- auth.config.ts
+++
@@ -1,8 +0,0 @@
-import type { NextAuthConfig } from "next-auth"
-import Google from "next-auth/providers/google"
-
-const authConfig = {
-  providers: [Google],
-} satisfies NextAuthConfig
-
-export default authConfig
--- auth.ts
+++ auth.ts
@@ -1,9 +1,10 @@
-import NextAuth from "next-auth"
-import { MongoDBAdapter } from "@auth/mongodb-adapter"
-import { clientPromise } from "./lib/db"
-import authConfig from "./auth.config"
 
+import NextAuth from "next-auth"
+import Google from "next-auth/providers/google"
+import { MongoDBAdapter } from "@auth/mongodb-adapter"
+import client from "./lib/db"
+ 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  ...authConfig,
-  adapter: MongoDBAdapter(clientPromise),
+  adapter: MongoDBAdapter(client),
+  providers: [Google],
 })
