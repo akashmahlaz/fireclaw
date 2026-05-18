db21ea73ea1b4955e91f2918fc5bd64134d75b5a
akashmahlaz <akashdalla408@gmail.com>
2026-05-18 14:03:30 +0530

    page adited

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,6 @@
 import Navbar from "@/components/layout/Navbar";
 import { AnnouncementBar } from "@/components/layout/AnnouncementBar";
 import { Hero } from "@/components/layout/Hero";
-import { CustomAgentStrip } from "@/components/layout/CustomAgentStrip";
-import { AgentTemplates } from "@/components/layout/AgentTemplates";
-import { Features } from "@/components/layout/Features";
-import { HowItWorks } from "@/components/layout/HowItWorks";
-import { Pricing } from "@/components/layout/Pricing";
 import { FAQ } from "@/components/layout/FAQ";
 import { CTA } from "@/components/layout/CTA";
 import { Footer } from "@/components/layout/Footer";
@@ -16,9 +11,6 @@ export default function Home() {
       <AnnouncementBar />
       <Navbar />
       <Hero />
-    
-      
-      
       <FAQ />
       <CTA />
       <Footer />
